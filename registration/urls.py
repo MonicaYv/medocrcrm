4b2b52651fef6ab205a2d78973eb82b5f2bfcd4b
Mona <monig5658@gmail.comm>
2025-06-25 16:28:51 +0530

    layout nd login changes

--- registration/urls.py
+++ registration/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('welcome', views.welcome, name='welcome'),
-    path('<str:role>/', views.register_by_role, name='register_by_role'),
-    path('save/user/', views.save_user, name='save_user'),
-    path('save/ngo/', views.save_ngo, name='save_ngo'),
-    path('login/', views.login_page, name='login_page'),
-    path('auth/login/', views.login_auth, name='login_auth'),
+    path('user/<str:role>', views.register_by_role, name='register_by_role'),
+    path('save/user', views.save_user, name='save_user'),
+    path('save/ngo', views.save_ngo, name='save_ngo'),
+    path('login', views.login_page, name='login_page'),
+    path('auth/login', views.login_auth, name='login_auth'),
 ]
