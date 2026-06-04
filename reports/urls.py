9305941e42efff8f08fa5edf53587a679eb9a7e7
syedamaan921-max <syedamaan921@gmail.com>
2026-06-04 17:30:55 +0530

    fix

--- reports/urls.py
+++ reports/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.reports, name='reports'),
     path("pharmacy-report-data/", views.pharmacy_report_data, name="pharmacy_report_data"),
     path("hospital-report-data/", views.hospital_report_data, name="hospital_report_data"),
+    path("doctor-report-data/", views.doctor_report_data, name="doctor_report_data"),
     path("lab-report-data/", views.lab_report_data, name="lab_report_data"),
     
 ]
