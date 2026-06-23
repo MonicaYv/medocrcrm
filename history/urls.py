4e42ec3ceacb98f5366ed89110f998eaeb50293c
MARUTI PAI <marutipai203@gmail.com>
2026-06-23 18:34:47 +0530

    fixed history listing

--- history/urls.py
+++ history/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("doctor/history/ajax/", views.ajax_doctor_history, name="ajax_doctor_history"),
     path("lab/history/ajax/", views.ajax_lab_history, name="ajax_lab_history"),
     path("hospital/history/ajax/", views.ajax_hospital_history, name="ajax_hospital_history"),
-    path("lab/complete/", views.complete_lab_appointment, name="complete_lab_appointment"),
-    path("lab/cancel/", views.cancel_lab_appointment, name="cancel_lab_appointment"),
+    path("cancel-bid/", views.cancel_bid, name="cancel_bid"),
+    path("complete-appointment/", views.complete_appointment, name="complete-appointment"),
+    path("no-show-appointment/", views.no_show_appointment, name="no-show-appointment"),
 ]
